ac82d44249f18d9e4aafe11709dc3b3aa3565f97
Karim <karim@MacBook-Air.local>
2026-08-27 12:45:54 +0200

    after-issues

--- rag_core.py
+++ rag_core.py
@@ -22,7 +22,7 @@ import time
 import hashlib
 import requests
 
-from geopy.distance import geodesic
+from streamlit_js_eval import get_geolocation
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_core.tools import tool
 from langchain_text_splitters import RecursiveCharacterTextSplitter
